15e468d164ed667c5b9fdfced93f3bd12aa6d4d0
Paloca-RP <ricardopalhoca@hotmail.com>
2020-12-16 18:56:35 +0000

    ol

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	printf("#-------------------------#");
+	printf("\n# 1- Visualização de mapa #");
+	printf("\n# 2- Reservar lote e      #");
+	printf("\n# inserir cliente         #");
+}
