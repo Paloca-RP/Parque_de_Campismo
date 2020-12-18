bd4a4b81431e4fe5d4226b574f87c35144bae5ba
Paloca-RP <73611099+Paloca-RP@users.noreply.github.com>
2020-12-18 20:15:39 +0000

    a

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,44 @@
 
 int main()
 {
+	char menu;
 	printf("#-------------------------#");
 	printf("\n# 1- Visualização de mapa #");
 	printf("\n# 2- Reservar lote e      #");
 	printf("\n# inserir cliente         #");
+	printf("\n#       6- Sair           #");
+    printf("\n#-------------------------#");
+    scanf(" %c", &menu);
+
+	switch(menu)
+	{
+		case 1:	
+			printf("ola");		
+			break;
+		
+		case 2:	
+			printf("ola");		
+			break;
+		
+		case 3:		
+			printf("ola");		
+			break;
+		
+		case 4:		
+			printf("ola");		
+			break;
+		
+		case 5:		
+			printf("ola");		
+			break;
+		
+		case 6:		
+			printf("ola");		
+			break;
+			
+		default:		
+			printf("ola");					
+	}
+
+
 }
