0a025fc4c4eee50f1636c85224eeaef65d412316
Paloca-RP <ricardopalhoca@hotmail.com>
2020-12-21 10:40:11 +0000

    menu -.-

--- Main.cpp
+++ Main.cpp
@@ -1,45 +1,62 @@
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 int main()
 {
+	setlocale(LC_ALL, "Portuguese");
+	
 	char menu;
-	printf("#-------------------------#");
-	printf("\n# 1- Visualização de mapa #");
-	printf("\n# 2- Reservar lote e      #");
-	printf("\n# inserir cliente         #");
-	printf("\n#       6- Sair           #");
-    printf("\n#-------------------------#");
-    scanf(" %c", &menu);
+	menu:
+	printf("  #-----------------------------------#\n");
+	printf("  #   Gestão de Parque de Campismo    #\n");
+	printf("  #-----------------------------------#\n");
+	printf("  #   1- Ver mapa de lotes            #\n");
+	printf("  #   2- Reservar um lote             #\n");
+	printf("  #   3- Consultar um lote            #\n");
+	printf("  #   4- Pesquisar lote por campista  #\n");
+	printf("  #   5- Editar um lote reservado     #\n");
+	printf("  #   6- Libertar um lote             #\n");
+	printf("  #   7- Sair                         #\n");
+    printf("  #-----------------------------------#\n");
+    printf("Insira uma opção:  ");
+	scanf("%c", &menu);
 
-	switch(menu)
-	{
-		case 1:	
-			printf("ola");		
-			break;
-		
-		case 2:	
-			printf("ola");		
+if(menu == 1)
+{
+	system("CLS");
+			printf("Ver mapa de lotes");
+}
+					
+			if(menu == 2)	
+			printf("Reservar um lote");		
 			break;
 		
 		case 3:		
-			printf("ola");		
+			printf("Consultar um lote");		
 			break;
 		
 		case 4:		
-			printf("ola");		
+			printf("Pesquisar lote por campista");		
 			break;
 		
 		case 5:		
-			printf("ola");		
+			printf("Editar um lote reservado");		
 			break;
 		
 		case 6:		
-			printf("ola");		
+			printf("Libertar um lote");		
+			break;
+			
+		case 7:
+			printf("Sair");
 			break;
 			
 		default:		
-			printf("ola");					
+			printf("Insira uma opção válida");
+			goto menu;					
 	}
 
 
