2d818c60bf61aed19e353908534c80b44585c5e6
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-01-06 13:38:50 +0000

    Create mapa estatico.cpp

---
+++ mapa estatico.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main (void )
+{
+  int matriz[5][5]={0},i, j;
+ 
+ matriz[0][0] = 'x';
+ 
+ 
+ 
+ 
+  printf("\n\n******************* Mapa ********************* \n\n");  
+  printf("   | 1 | 2 | 3 | 4 | 5 \n");
+  for ( i=0; i<5; i++ )
+  {
+    printf("%d -",i+1);
+	    for ( j=0; j<5; j++ )
+	    {
+	      printf ("| %c ",matriz[ i ][ j ]);
+	    }
+	    printf("\n");
+  }
+  
+  getch();
+  return(0);
+}
+
